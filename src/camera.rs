e9909bda67da30244bb1f801a119febd493accfb
YushiOMOTE <yushiomote@gmail.com>
2022-08-07 22:20:17 +0900

    Adjust camera position

--- src/camera.rs
+++ src/camera.rs
@@ -11,7 +11,7 @@ pub fn update_camera(position: &Position, size: &Size, _: &Camera) {
     let target = **position + **size * 0.5;
 
     set_camera(&Camera3D {
-        position: vec3(target.x - 30.0, target.y - 50.0, target.z + 80.0),
+        position: vec3(target.x - 30.0, target.y - 50.0, target.z + 120.0),
         up: vec3(0., 0., 1.),
         target,
         ..Default::default()
